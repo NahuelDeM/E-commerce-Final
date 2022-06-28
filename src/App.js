bd3d051915c387d6b8a126452a0f7a1192548339
NahuelDeM <101424592+NahuelDeM@users.noreply.github.com>
2022-06-28 16:06:35 -0300

    Nahuel

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Producto from "./Componentes/Producto";
+
+function App() {
+  return (
+    <div className="App">
+      rendering
+      <Producto /> 
+    </div>
+  );
+}
+
+export default App;
